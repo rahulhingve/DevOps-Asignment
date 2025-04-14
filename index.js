9cabe407e5d928bbdf6cdd76fe9cee982c536692
rahulhingve <rahulpawar2001.rp@gmail.com>
2025-04-14 17:41:26 +0530

    added prometheus metrics

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const express = require("express");
-
+const client = require('prom-client');
 
 const app = express();
-
+const port = 3100;
+const metricsPort = 3101;
 
 app.get("/", (req , res )=>{
     res.send("Hello From Digital Ocean Server Coz mine azure free tier expired🥲<br>A small change to test the deployment");
 })
 
 
-app.listen(3100, ()=>{
+// Prom Metrics
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
+app.get("/metrics", async (req, res) => {
+    res.set("Content-Type", register.contentType);
+    res.end(await register.metrics());
+})
+
+
+app.listen(port, ()=>{
     console.log("Server is running on port 3100");
+    require('http').createServer(app).listen(metricsPort, ()=>{
+        console.log("Metrics server is running on port 3101");
+    })
 })
 
 
